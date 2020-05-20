60c4bdfd703ab6b6e51204b306309ef85d4c2ba5
tchoward <64616204+tchoward@users.noreply.github.com>
2020-05-19 21:00:29 -0400

    Added Hubi Gauge

--- hubigraphs.groovy
+++ hubigraphs.groovy
@@ -1,19 +1,3 @@
-/**
- *  Hubigraph Parent App
- *
- *  Copyright 2020, but let's behonest, you'll copy it
- *
- *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- *  in compliance with the License. You may obtain a copy of the License at:
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
- *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
- *  for the specific language governing permissions and limitations under the License.
- *
- */
-
 definition(
     name: "Hubigraphs",
     namespace: "tchoward",
@@ -31,6 +15,7 @@ preferences {
         section {
             app(name: "hubiGraphLine", appName: "Hubigraph Line Graph", namespace: "tchoward", title: "Create New Line Graph", multiple: true)
 			app(name: "hubiGraphTime", appName: "Hubigraph Time Line", namespace: "tchoward", title: "Create New TimeLine", multiple: true)
+            app(name: "hubiGauge", appName: "Hubigraph Gauge", namespace: "tchoward", title: "Create New Gauge", multiple: true)
         }
     }
 }
